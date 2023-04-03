bbdce8e66ad677c0ab76b49d84b87c957c7a547b
AadityaMall <aadityarmall@gmail.com>
2023-04-03 23:07:11 +0530

    User Page
    User Page front end complete

--- ECommerceWebsite/Scripts/profile.js
+++ ECommerceWebsite/Scripts/profile.js
@@ -1,1 +1,18 @@
-﻿
+﻿function darkMode() {
+    var userPageCard = document.getElementById("userProfileCard");
+    var usernameHead = document.getElementById("UserNameExtract");
+    usernameHead.style.color = "white"
+    userPageCard.classList.add("bg-dark");
+    var signOutButton = document.getElementById("signOutButton");
+    signOutButton.classList.add("btn-dark");
+    
+}
+
+function lightMode() {
+    var userPageCard = document.getElementById("userProfileCard");
+    var usernameHead = document.getElementById("UserNameExtract");
+    usernameHead.style.color = "black";
+    userPageCard.classList.remove("bg-dark");
+    var signOutButton = document.getElementById("signOutButton");
+    signOutButton.classList.remove("btn-dark");
+}
