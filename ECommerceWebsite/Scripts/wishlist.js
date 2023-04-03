63ca95163fc0f0f4b1bee0d21b2b7146f3756200
AadityaMall <aadityarmall@gmail.com>
2023-04-03 16:35:21 +0530

    Added Wishlist to front end
    updated html css and js file

--- ECommerceWebsite/Scripts/wishlist.js
+++ ECommerceWebsite/Scripts/wishlist.js
@@ -1,1 +1,34 @@
-﻿
+﻿var c = document.getElementById("wishlistProducts");
+
+function calcQuantity() {
+    var counter = 0;
+    var el = document.getElementById("wishlistItemCount");
+    for (let i = 0; i < c.children.length; i++) {
+        if (c.children[i].classList.contains("deleted")) {
+            continue
+        }
+        else {
+            counter = counter + 1;
+        }
+    }
+    if (counter == 0) {
+        el.innerHTML = "YOUR WISHLIST IS EMPTY, SHOP NOW!";
+
+    }
+    else {
+        if (counter > 1) {
+            el.innerHTML = counter + " Items";
+        }
+        else {
+            el.innerHTML = counter + " Item";
+        }
+
+    }
+}
+
+function deleteItemFromwishlist(id) {
+    id.parentElement.classList.add("deleted");
+    calcQuantity();
+}
+
+calcQuantity();
